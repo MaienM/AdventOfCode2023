2d8d655044dba14414ef9fcdf491a5ad9a10e1cc
Michon van Dooren <me@maienm.com>
2023-12-03 10:19:28 +0100

    Add tests for point lib

--- src/point.rs
+++ src/point.rs
@@ -7,7 +7,7 @@ use derive_new::new;
 
 // Implements an operator (add/sub/mul/div) for a point type, including the assign variant of the operator.
 macro_rules! impl_operator {
-    ($name:ident, $op:ident, $($vars:ident),+) => {
+    ($name:ident, $op:ident, $($var:ident),+) => {
         paste::paste! {
             impl<T, R> [<$op:camel>]<$name<R>> for $name<T>
             where
@@ -15,9 +15,10 @@ macro_rules! impl_operator {
                 <T as [<$op:camel>]<R>>::Output: Into<T>,
             {
                 type Output = Self;
+                #[must_use]
                 fn [<$op>](self, rhs: $name<R>) -> Self {
                     Self {
-                        $($vars: self.$vars.[<$op>](rhs.$vars).into()),+
+                        $($var: self.$var.[<$op>](rhs.$var).into()),+
                     }
                 }
             }
@@ -28,7 +29,7 @@ macro_rules! impl_operator {
             {
                 fn [<$op _assign>](&mut self, rhs: $name<R>) {
                     *self = Self {
-                        $($vars: self.$vars.[<$op>](rhs.$vars).into()),+
+                        $($var: self.$var.[<$op>](rhs.$var).into()),+
                     };
                 }
             }
@@ -38,15 +39,16 @@ macro_rules! impl_operator {
 
 // Generate a point class with the given name and variables.
 macro_rules! create_point {
-    ($name:ident, $($vars:ident),+) => {
+    ($name:ident, $($var:ident),+) => {
         #[derive(Clone, Copy, Eq, Hash, PartialEq, new)]
         pub struct $name<T = usize> {
-            $(pub $vars: T),+
+            $(pub $var: T),+
         }
-        impl_operator!($name, add, $($vars),+);
-        impl_operator!($name, sub, $($vars),+);
-        impl_operator!($name, mul, $($vars),+);
-        impl_operator!($name, div, $($vars),+);
+
+        impl_operator!($name, add, $($var),+);
+        impl_operator!($name, sub, $($var),+);
+        impl_operator!($name, mul, $($var),+);
+        impl_operator!($name, div, $($var),+);
     };
 }
 
@@ -61,5 +63,92 @@ impl<T: Debug> Debug for Point2<T> {
 impl<T: Debug> Debug for Point3<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("Point({:?}, {:?}, {:?})", self.x, self.y, self.z))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn add() {
+        assert_eq!(Point2::new(10, 5) + Point2::new(8, 7), Point2::new(18, 12));
+        assert_eq!(
+            Point3::new(10, 5, 7) + Point3::new(8, 7, 1),
+            Point3::new(18, 12, 8)
+        );
+    }
+
+    #[test]
+    fn add_assign() {
+        let mut point = Point2::new(10, 5);
+        point += Point2::new(8, 7);
+        assert_eq!(point, Point2::new(18, 12));
+
+        let mut point = Point3::new(10, 5, 7);
+        point += Point3::new(8, 7, 1);
+        assert_eq!(point, Point3::new(18, 12, 8));
+    }
+
+    #[test]
+    fn sub() {
+        assert_eq!(Point2::new(10, 5) - Point2::new(8, 2), Point2::new(2, 3));
+        assert_eq!(
+            Point3::new(10, 5, 7) - Point3::new(8, 2, 1),
+            Point3::new(2, 3, 6)
+        );
+    }
+
+    #[test]
+    fn sub_assign() {
+        let mut point = Point2::new(10, 5);
+        point -= Point2::new(8, 2);
+        assert_eq!(point, Point2::new(2, 3));
+
+        let mut point = Point3::new(10, 5, 7);
+        point -= Point3::new(8, 2, 1);
+        assert_eq!(point, Point3::new(2, 3, 6));
+    }
+
+    #[test]
+    fn mul() {
+        assert_eq!(Point2::new(10, 5) * Point2::new(2, 3), Point2::new(20, 15));
+        assert_eq!(
+            Point3::new(10, 5, 7) * Point3::new(2, 3, 4),
+            Point3::new(20, 15, 28)
+        );
+    }
+
+    #[test]
+    fn mul_assign() {
+        let mut point = Point2::new(10, 5);
+        point *= Point2::new(2, 3);
+        assert_eq!(point, Point2::new(20, 15));
+
+        let mut point = Point3::new(10, 5, 7);
+        point *= Point3::new(2, 3, 4);
+        assert_eq!(point, Point3::new(20, 15, 28));
+    }
+
+    #[test]
+    fn div() {
+        assert_eq!(Point2::new(20, 15) / Point2::new(2, 3), Point2::new(10, 5));
+        assert_eq!(
+            Point3::new(20, 15, 28) / Point3::new(2, 3, 4),
+            Point3::new(10, 5, 7)
+        );
+    }
+
+    #[test]
+    fn div_assign() {
+        let mut point = Point2::new(20, 15);
+        point /= Point2::new(2, 3);
+        assert_eq!(point, Point2::new(10, 5));
+
+        let mut point = Point3::new(20, 15, 28);
+        point /= Point3::new(2, 3, 4);
+        assert_eq!(point, Point3::new(10, 5, 7));
     }
 }
