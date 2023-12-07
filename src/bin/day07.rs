2e0fe55e764eb157a076d7882207be91a6fd006e
Michon van Dooren <me@maienm.com>
2023-12-07 07:04:58 +0100

    Clean up day07

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,12 +1,6 @@
 use aoc::{generate_day_main, splitn};
 
-type Card = u8;
-
-const CARD_A: u8 = 14;
-const CARD_K: u8 = 13;
-const CARD_Q: u8 = 12;
-const CARD_J: u8 = 1;
-const CARD_T: u8 = 10;
+type Card = char;
 
 #[derive(Debug, PartialEq)]
 struct Hand {
@@ -19,125 +13,134 @@ fn parse_input(input: &str) -> Vec<Hand> {
         .split('\n')
         .map(|line| {
             let (cards, bid) = splitn!(line, ' ', str, usize);
-            let cards = cards
-                .chars()
-                .map(|c| match c {
-                    'A' => CARD_A,
-                    'K' => CARD_K,
-                    'Q' => CARD_Q,
-                    'J' => CARD_J,
-                    'T' => CARD_T,
-                    c => c.to_digit(10).unwrap() as u8,
-                })
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
+            let cards = cards.chars().collect::<Vec<_>>().try_into().unwrap();
             Hand { cards, bid }
         })
         .collect()
 }
 
-pub fn part1(input: &str) -> usize {
-    let hands = parse_input(input);
+fn card_to_numbers(card: Card, value_for_j: u8) -> u8 {
+    match card {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => value_for_j,
+        'T' => 10,
+        c => c.to_digit(10).unwrap() as u8,
+    }
+}
+
+fn cards_to_numbers(cards: [Card; 5], value_for_j: u8) -> [u8; 5] {
+    [
+        card_to_numbers(cards[0], value_for_j),
+        card_to_numbers(cards[1], value_for_j),
+        card_to_numbers(cards[2], value_for_j),
+        card_to_numbers(cards[3], value_for_j),
+        card_to_numbers(cards[4], value_for_j),
+    ]
+}
+
+fn calculate_total_winnings(hands: Vec<Hand>, calculate_score: fn(Hand) -> (u8, [u8; 5])) -> usize {
     let mut scores: Vec<_> = hands
         .into_iter()
         .map(|hand| {
-            let mut groups = [0usize; 15];
-            for card in hand.cards {
-                groups[card as usize] += 1;
-            }
-            groups.sort_unstable();
-            groups.reverse();
-            let typ = match (groups[0], groups[1]) {
-                (5, _) => 6,
-                (4, _) => 5,
-                (3, 2) => 4,
-                (3, _) => 3,
-                (2, 2) => 2,
-                (2, _) => 1,
-                (_, _) => 0,
-            };
-
-            let score = typ * 15usize.pow(5)
-                + hand.cards[0] as usize * 15usize.pow(4)
-                + hand.cards[1] as usize * 15usize.pow(3)
-                + hand.cards[2] as usize * 15usize.pow(2)
-                + hand.cards[3] as usize * 15usize
-                + hand.cards[4] as usize;
-
-            (score, hand.bid)
+            let bid = hand.bid;
+            let (typ, cards) = calculate_score(hand);
+            let score = typ as usize * 15usize.pow(5)
+                + cards[0] as usize * 15usize.pow(4)
+                + cards[1] as usize * 15usize.pow(3)
+                + cards[2] as usize * 15usize.pow(2)
+                + cards[3] as usize * 15usize
+                + cards[4] as usize;
+            (score, bid)
         })
         .collect();
-    scores.sort_unstable_by_key(|p| p.0);
+    scores.sort_unstable_by_key(|(score, _)| *score);
     let scores: Vec<_> = scores
         .into_iter()
         .enumerate()
-        .map(|(r, (_, b))| (r + 1) * b)
+        .map(|(rank, (_, bid))| (rank + 1) * bid)
         .collect();
     scores.iter().sum()
 }
 
+pub fn part1(input: &str) -> usize {
+    let hands = parse_input(input);
+    calculate_total_winnings(hands, |hand| {
+        let mut groups = [0usize; 15];
+        let cards = cards_to_numbers(hand.cards, 11);
+        for card in cards {
+            groups[card as usize] += 1;
+        }
+        groups.sort_unstable();
+
+        let typ = match (groups[14], groups[13]) {
+            // Five of a kind.
+            (5, _) => 6,
+            // Four of a kind.
+            (4, _) => 5,
+            // Full house.
+            (3, 2) => 4,
+            // Three of a kind.
+            (3, _) => 3,
+            // Two pair.
+            (2, 2) => 2,
+            // One pair.
+            (2, _) => 1,
+            // High card.
+            (_, _) => 0,
+        };
+
+        (typ, cards)
+    })
+}
+
 pub fn part2(input: &str) -> usize {
     let hands = parse_input(input);
-    let mut scores: Vec<_> = hands
-        .into_iter()
-        .map(|hand| {
-            let mut groups = [0usize; 15];
-            let mut jokers = 0;
-            for card in hand.cards {
-                match card {
-                    CARD_J => jokers += 1,
-                    card => groups[card as usize] += 1,
-                };
-            }
-            groups.sort_unstable();
-            groups.reverse();
-            let typ = match (jokers, groups[0], groups[1]) {
-                // Five of a kind.
-                (0, 5, _) => 6,
-                (1, 4, _) => 6,
-                (2, 3, _) => 6,
-                (3, 2, _) => 6,
-                (4, 1, _) => 6,
-                (5, _, _) => 6,
-                // Four of a kind.
-                (0, 4, _) => 5,
-                (1, 3, _) => 5,
-                (2, 2, _) => 5,
-                (3, 1, _) => 5,
-                // Full house.
-                (0, 3, 2) => 4,
-                (1, 2, 2) => 4,
-                // Three of a kind>
-                (0, 3, _) => 3,
-                (1, 2, _) => 3,
-                (2, 1, _) => 3,
-                // Two pair.
-                (0, 2, 2) => 2,
-                // One pair.
-                (0, 2, _) => 1,
-                (1, 1, _) => 1,
-                // High card.
-                (_, _, _) => 0,
+    calculate_total_winnings(hands, |hand| {
+        let mut groups = [0usize; 15];
+        let mut jokers = 0;
+        let cards = cards_to_numbers(hand.cards, 0);
+        for card in cards {
+            match card {
+                0 => jokers += 1,
+                card => groups[card as usize] += 1,
             };
+        }
+        groups.sort_unstable();
 
-            let score = typ * 15usize.pow(5)
-                + hand.cards[0] as usize * 15usize.pow(4)
-                + hand.cards[1] as usize * 15usize.pow(3)
-                + hand.cards[2] as usize * 15usize.pow(2)
-                + hand.cards[3] as usize * 15usize
-                + hand.cards[4] as usize;
+        #[allow(clippy::match_same_arms)]
+        let typ = match (jokers, groups[14], groups[13]) {
+            // Five of a kind.
+            (0, 5, _) => 6,
+            (1, 4, _) => 6,
+            (2, 3, _) => 6,
+            (3, 2, _) => 6,
+            (4, 1, _) => 6,
+            (5, _, _) => 6,
+            // Four of a kind.
+            (0, 4, _) => 5,
+            (1, 3, _) => 5,
+            (2, 2, _) => 5,
+            (3, 1, _) => 5,
+            // Full house.
+            (0, 3, 2) => 4,
+            (1, 2, 2) => 4,
+            // Three of a kind>
+            (0, 3, _) => 3,
+            (1, 2, _) => 3,
+            (2, 1, _) => 3,
+            // Two pair.
+            (0, 2, 2) => 2,
+            // One pair.
+            (0, 2, _) => 1,
+            (1, 1, _) => 1,
+            // High card.
+            (_, _, _) => 0,
+        };
 
-            (score, hand.bid)
-        })
-        .collect();
-    scores.sort_unstable_by_key(|p| p.0);
-    let scores: Vec<_> = scores
-        .into_iter()
-        .enumerate()
-        .map(|(r, (_, b))| (r + 1) * b)
-        .collect();
-    scores.iter().sum()
+        (typ, cards)
+    })
 }
 
 generate_day_main!(part1, part2);
@@ -164,23 +167,23 @@ mod tests {
         let actual = parse_input(&EXAMPLE_INPUT);
         let expected = vec![
             Hand {
-                cards: [3, 2, CARD_T, 3, CARD_K],
+                cards: ['3', '2', 'T', '3', 'K'],
                 bid: 765,
             },
             Hand {
-                cards: [CARD_T, 5, 5, CARD_J, 5],
+                cards: ['T', '5', '5', 'J', '5'],
                 bid: 684,
             },
             Hand {
-                cards: [CARD_K, CARD_K, 6, 7, 7],
+                cards: ['K', 'K', '6', '7', '7'],
                 bid: 28,
             },
             Hand {
-                cards: [CARD_K, CARD_T, CARD_J, CARD_J, CARD_T],
+                cards: ['K', 'T', 'J', 'J', 'T'],
                 bid: 220,
             },
             Hand {
-                cards: [CARD_Q, CARD_Q, CARD_Q, CARD_J, CARD_A],
+                cards: ['Q', 'Q', 'Q', 'J', 'A'],
                 bid: 483,
             },
         ];
