8795413c08168a7dac00bc77493ac1c6c194b741
Michon van Dooren <me@maienm.com>
2023-12-07 06:55:47 +0100

    Implement day07-2
    This is _very_ quick-and-dirty and breaks part 1 in the progress.

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -5,7 +5,7 @@ type Card = u8;
 const CARD_A: u8 = 14;
 const CARD_K: u8 = 13;
 const CARD_Q: u8 = 12;
-const CARD_J: u8 = 11;
+const CARD_J: u8 = 1;
 const CARD_T: u8 = 10;
 
 #[derive(Debug, PartialEq)]
@@ -77,7 +77,70 @@ pub fn part1(input: &str) -> usize {
     scores.iter().sum()
 }
 
-generate_day_main!(part1);
+pub fn part2(input: &str) -> usize {
+    let hands = parse_input(input);
+    let mut scores: Vec<_> = hands
+        .into_iter()
+        .map(|hand| {
+            let mut groups = [0usize; 15];
+            let mut jokers = 0;
+            for card in hand.cards {
+                match card {
+                    CARD_J => jokers += 1,
+                    card => groups[card as usize] += 1,
+                };
+            }
+            groups.sort_unstable();
+            groups.reverse();
+            let typ = match (jokers, groups[0], groups[1]) {
+                // Five of a kind.
+                (0, 5, _) => 6,
+                (1, 4, _) => 6,
+                (2, 3, _) => 6,
+                (3, 2, _) => 6,
+                (4, 1, _) => 6,
+                (5, _, _) => 6,
+                // Four of a kind.
+                (0, 4, _) => 5,
+                (1, 3, _) => 5,
+                (2, 2, _) => 5,
+                (3, 1, _) => 5,
+                // Full house.
+                (0, 3, 2) => 4,
+                (1, 2, 2) => 4,
+                // Three of a kind>
+                (0, 3, _) => 3,
+                (1, 2, _) => 3,
+                (2, 1, _) => 3,
+                // Two pair.
+                (0, 2, 2) => 2,
+                // One pair.
+                (0, 2, _) => 1,
+                (1, 1, _) => 1,
+                // High card.
+                (_, _, _) => 0,
+            };
+
+            let score = typ * 15usize.pow(5)
+                + hand.cards[0] as usize * 15usize.pow(4)
+                + hand.cards[1] as usize * 15usize.pow(3)
+                + hand.cards[2] as usize * 15usize.pow(2)
+                + hand.cards[3] as usize * 15usize
+                + hand.cards[4] as usize;
+
+            (score, hand.bid)
+        })
+        .collect();
+    scores.sort_unstable_by_key(|p| p.0);
+    let scores: Vec<_> = scores
+        .into_iter()
+        .enumerate()
+        .map(|(r, (_, b))| (r + 1) * b)
+        .collect();
+    scores.iter().sum()
+}
+
+generate_day_main!(part1, part2);
 
 #[cfg(test)]
 mod tests {
@@ -127,5 +190,10 @@ mod tests {
     #[test]
     fn example_part1() {
         assert_eq!(part1(&EXAMPLE_INPUT), 6440);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(&EXAMPLE_INPUT), 5905);
     }
 }
