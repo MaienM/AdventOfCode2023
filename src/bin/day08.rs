c8e58c67d2d72f57b7a6850b3e7f4cedbe662a97
Michon van Dooren <me@maienm.com>
2023-12-08 07:16:00 +0100

    Implement day08-2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use aoc::{generate_day_main, splitn};
 
@@ -36,22 +36,83 @@ fn parse_input(input: &str) -> Instructions {
     }
 }
 
-pub fn part1(input: &str) -> usize {
-    let instructions = parse_input(input);
-    let mut current = "AAA";
+fn run_until<'a>(
+    instructions: &Instructions<'a>,
+    offset: usize,
+    start: &'a str,
+    is_end: fn(&str) -> bool,
+) -> (usize, &'a str) {
+    let mut current = start;
     let mut steps = 0;
-    for direction in instructions.directions.iter().cloned().cycle() {
-        current = instructions.maps.get(current).unwrap()[direction as usize];
+    for direction in instructions
+        .directions
+        .iter()
+        .cloned()
+        .cycle()
+        .skip(offset % instructions.directions.len())
+    {
+        let idx = direction as usize;
         steps += 1;
+        current = instructions.maps.get(current).unwrap()[idx];
 
-        if current == "ZZZ" {
-            return steps;
+        if is_end(current) {
+            return (steps, current);
         }
     }
-    0
+    panic!("Should never happen");
 }
 
-generate_day_main!(part1);
+fn prime_factors(num: usize) -> HashSet<usize> {
+    let mut current = num;
+    let mut factors = HashSet::new();
+    for i in 2.. {
+        while current % i == 0 {
+            current /= i;
+            factors.insert(i);
+        }
+        if current == 1 {
+            break;
+        }
+    }
+    factors
+}
+
+fn least_common_multiple(mut numbers: Vec<usize>) -> usize {
+    match (numbers.pop(), numbers.pop()) {
+        (Some(a), Some(b)) => {
+            let factors_a = prime_factors(a);
+            let factors_b = prime_factors(b);
+            let gcf = factors_a.intersection(&factors_b).max().unwrap_or(&1);
+            numbers.push(a * b / gcf);
+            least_common_multiple(numbers)
+        }
+        (Some(a), None) => a,
+        _ => panic!("Should never happen"),
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let instructions = parse_input(input);
+    run_until(&instructions, 0, "AAA", |c| c == "ZZZ").0
+}
+
+pub fn part2(input: &str) -> usize {
+    let instructions = parse_input(input);
+    let cycles: Vec<_> = instructions
+        .maps
+        .keys()
+        .filter(|k| k.ends_with('A'))
+        .map(|k| {
+            let (first, k) = run_until(&instructions, 0, k, |c| c.ends_with('Z'));
+            let (cycle, _) = run_until(&instructions, first, k, |c| c.ends_with('Z'));
+            assert_eq!(first, cycle, "This logic only works if start -> finish and finish -> finish take the same amount of steps.");
+            first
+        })
+        .collect();
+    least_common_multiple(cycles)
+}
+
+generate_day_main!(part1, part2);
 
 #[cfg(test)]
 mod tests {
@@ -83,6 +144,20 @@ mod tests {
         ZZZ = (ZZZ, ZZZ)
     ";
 
+    #[example_input(part2 = 6, test)]
+    static EXAMPLE_INPUT_3: &str = "
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+    ";
+
     #[test]
     fn example_parse_1() {
         let actual = parse_input(&EXAMPLE_INPUT_1);
@@ -110,6 +185,25 @@ mod tests {
                 "AAA" => ["BBB", "BBB"],
                 "BBB" => ["AAA", "ZZZ"],
                 "ZZZ" => ["ZZZ", "ZZZ"],
+            ),
+        };
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn example_parse_3() {
+        let actual = parse_input(&EXAMPLE_INPUT_3);
+        let expected = Instructions {
+            directions: vec![Direction::Left, Direction::Right],
+            maps: hash_map!(
+                "11A" => ["11B", "XXX"],
+                "11B" => ["XXX", "11Z"],
+                "11Z" => ["11B", "XXX"],
+                "22A" => ["22B", "XXX"],
+                "22B" => ["22C", "22C"],
+                "22C" => ["22Z", "22Z"],
+                "22Z" => ["22B", "22B"],
+                "XXX" => ["XXX", "XXX"],
             ),
         };
         assert_eq!(actual, expected);
