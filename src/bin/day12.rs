d5e28209d6b8b7473a433f64baff5118cc4cb460
Michon van Dooren <me@maienm.com>
2023-12-12 17:45:04 +0100

    Implement day12-2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use aoc::utils::parse::splitn;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Condition {
     Operational,
     Damaged,
@@ -17,7 +19,7 @@ impl From<char> for Condition {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 struct Record {
     conditions: Vec<Condition>,
     damaged_groups: Vec<usize>,
@@ -36,53 +38,108 @@ fn parse_input(input: &str) -> Vec<Record> {
         .collect()
 }
 
-fn _find_valid_options(record: &mut Record, indexes: &[usize], index_of_indexes: usize) -> usize {
-    if let Some(index) = indexes.get(index_of_indexes) {
-        record.conditions[*index] = Condition::Operational;
-        let count_a = _find_valid_options(record, indexes, index_of_indexes + 1);
-        record.conditions[*index] = Condition::Damaged;
-        let count_b = _find_valid_options(record, indexes, index_of_indexes + 1);
-        count_a + count_b
-    } else {
-        let mut groups = Vec::new();
-        let mut last = &Condition::Operational;
-        let mut len = 0;
-        for condition in &record.conditions {
-            match (last, condition) {
-                (Condition::Damaged, Condition::Operational) => {
-                    groups.push(len);
-                    len = 0;
-                }
-                (_, Condition::Damaged) => {
-                    len += 1;
-                }
-                _ => {}
-            }
-            last = condition;
-        }
-        if len > 0 {
-            groups.push(len);
-        }
-        usize::from(groups == record.damaged_groups)
+fn find_valid_options(
+    cache: &mut HashMap<(usize, usize, usize), usize>,
+    record: &mut Record,
+    index: usize,
+    groups_index: usize,
+    current_group_size: usize,
+) -> usize {
+    let cache_key = (index, groups_index, current_group_size);
+    if let Some(result) = cache.get(&cache_key) {
+        return *result;
     }
-}
 
-fn find_valid_options(record: &mut Record) -> usize {
-    let indexes: Vec<_> = record
-        .conditions
-        .iter()
-        .enumerate()
-        .filter(|(_, c)| c == &&Condition::Unknown)
-        .map(|(i, _)| i)
-        .collect();
-    _find_valid_options(record, &indexes, 0)
+    let condition = match record.conditions.get(index) {
+        Some(Condition::Unknown) if current_group_size > 0 => {
+            if &current_group_size < record.damaged_groups.get(groups_index).unwrap_or(&0) {
+                Some(&Condition::Damaged)
+            } else {
+                Some(&Condition::Operational)
+            }
+        }
+        None if current_group_size > 0 => Some(&Condition::Operational),
+        condition => condition,
+    };
+    let result = match condition {
+        Some(Condition::Operational) => {
+            if current_group_size > 0 {
+                if Some(&current_group_size) == record.damaged_groups.get(groups_index) {
+                    find_valid_options(cache, record, index + 1, groups_index + 1, 0)
+                } else {
+                    0
+                }
+            } else {
+                find_valid_options(cache, record, index + 1, groups_index, current_group_size)
+            }
+        }
+        Some(Condition::Damaged) => {
+            if &current_group_size >= record.damaged_groups.get(groups_index).unwrap_or(&0) {
+                0
+            } else {
+                find_valid_options(
+                    cache,
+                    record,
+                    index + 1,
+                    groups_index,
+                    current_group_size + 1,
+                )
+            }
+        }
+        Some(Condition::Unknown) => {
+            find_valid_options(cache, record, index + 1, groups_index, current_group_size)
+                + find_valid_options(
+                    cache,
+                    record,
+                    index + 1,
+                    groups_index,
+                    current_group_size + 1,
+                )
+        }
+        None => usize::from(groups_index == record.damaged_groups.len()),
+    };
+    cache.insert(cache_key, result);
+    result
 }
 
 pub fn part1(input: &str) -> usize {
     let records = parse_input(input);
     records
         .into_iter()
-        .map(|mut record| find_valid_options(&mut record))
+        .map(|mut record| find_valid_options(&mut HashMap::new(), &mut record, 0, 0, 0))
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let records = parse_input(input);
+    records
+        .into_iter()
+        .map(|record| {
+            let mut conditions = Vec::new();
+            conditions.append(&mut record.conditions.clone());
+            conditions.push(Condition::Unknown);
+            conditions.append(&mut record.conditions.clone());
+            conditions.push(Condition::Unknown);
+            conditions.append(&mut record.conditions.clone());
+            conditions.push(Condition::Unknown);
+            conditions.append(&mut record.conditions.clone());
+            conditions.push(Condition::Unknown);
+            conditions.append(&mut record.conditions.clone());
+
+            let mut damaged_groups = Vec::new();
+            damaged_groups.append(&mut record.damaged_groups.clone());
+            damaged_groups.append(&mut record.damaged_groups.clone());
+            damaged_groups.append(&mut record.damaged_groups.clone());
+            damaged_groups.append(&mut record.damaged_groups.clone());
+            damaged_groups.append(&mut record.damaged_groups.clone());
+
+            let mut record = Record {
+                conditions,
+                damaged_groups,
+            };
+
+            find_valid_options(&mut HashMap::new(), &mut record, 0, 0, 0)
+        })
         .sum()
 }
 
@@ -95,13 +152,43 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 21, test)]
+    #[example_input(part1 = 21, part2 = 525_152, test)]
     static EXAMPLE_INPUT: &str = "
         ???.### 1,1,3
         .??..??...?##. 1,1,3
         ?#?#?#?#?#?#?#? 1,3,1,6
         ????.#...#... 4,1,1
         ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
+    ";
+
+    #[example_input(part1 = 1, part2 = 1, test)]
+    static EXAMPLE_INPUT_LINE1: &str = "
+        ???.### 1,1,3
+    ";
+
+    #[example_input(part1 = 4, part2 = 16_384, test)]
+    static EXAMPLE_INPUT_LINE2: &str = "
+        .??..??...?##. 1,1,3
+    ";
+
+    #[example_input(part1 = 1, part2 = 1, test)]
+    static EXAMPLE_INPUT_LINE3: &str = "
+        ?#?#?#?#?#?#?#? 1,3,1,6
+    ";
+
+    #[example_input(part1 = 1, part2 = 16, test)]
+    static EXAMPLE_INPUT_LINE4: &str = "
+        ????.#...#... 4,1,1
+    ";
+
+    #[example_input(part1 = 4, part2 = 2500, test)]
+    static EXAMPLE_INPUT_LINE5: &str = "
+        ????.######..#####. 1,6,5
+    ";
+
+    #[example_input(part1 = 10, part2 = 506_250, test)]
+    static EXAMPLE_INPUT_LINE6: &str = "
         ?###???????? 3,2,1
     ";
 
