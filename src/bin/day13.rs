2a85c6302221cbb0884695e7c891a4cc155e67a2
Michon van Dooren <me@maienm.com>
2023-12-13 06:36:44 +0100

    Implement day13-2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -18,32 +18,53 @@ fn rotate(map: &Map) -> Map {
         .collect()
 }
 
-fn find_reflection_row(map: &Map) -> Option<usize> {
+fn find_reflection_row(map: &Map, with_smudge: bool) -> Option<usize> {
     let len = map.len();
     'row: for i in 0..(len - 1) {
+        let mut found_smudge = !with_smudge;
         for o in 0..=i.min(len - i - 2) {
-            if map[i - o] != map[i + o + 1] {
-                continue 'row;
-            }
+            let left = &map[i - o];
+            let right = &map[i + o + 1];
+            let diff = (0..left.len()).find(|i| left[*i] != right[*i]);
+            match diff {
+                None => {}
+                Some(idx) if !found_smudge => {
+                    found_smudge = true;
+                    if ((idx + 1)..left.len()).any(|i| left[i] != right[i]) {
+                        continue 'row;
+                    }
+                }
+                Some(_) => continue 'row,
+            };
         }
-        return Some(i);
+        if found_smudge {
+            return Some(i);
+        }
     }
     None
 }
 
-pub fn part1(input: &str) -> usize {
+fn solve(input: &str, with_smudge: bool) -> usize {
     let maps = parse_input(input);
     maps.into_iter()
         .map(|map| {
-            if let Some(row) = find_reflection_row(&map) {
+            if let Some(row) = find_reflection_row(&map, with_smudge) {
                 (row + 1) * 100
             } else {
                 let map = rotate(&map);
-                let column = find_reflection_row(&map).unwrap();
+                let column = find_reflection_row(&map, with_smudge).unwrap();
                 column + 1
             }
         })
         .sum()
+}
+
+pub fn part1(input: &str) -> usize {
+    solve(input, false)
+}
+
+pub fn part2(input: &str) -> usize {
+    solve(input, true)
 }
 
 aoc::cli::single::generate_main!();
@@ -55,7 +76,7 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 405, test)]
+    #[example_input(part1 = 405, part2 = 400, test)]
     static EXAMPLE_INPUT: &str = "
         #.##..##.
         ..#.##.#.
